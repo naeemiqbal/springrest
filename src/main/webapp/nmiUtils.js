c00c078ba26c92b6504e96545e4a498a87d7db91
Naeem Iqbal <naeem.iqbal@infor.com>
2022-12-23 16:50:57 -0500

    12/23

---
+++ src/main/webapp/nmiUtils.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+
+function getTable(data) {
+	var tbl = "<table>",
+		cols = [], siz = 0;
+	if (Array.isArray(data)) {
+		let r = data[0];
+		tbl += "<thead><tr>";
+		for (let col in r) {
+			tbl += "<td>" + col + "</td>";
+			cols.push(col);
+			siz++;
+		}
+		tbl += "</tr></thead><tbody>";
+		data.forEach(itm => {
+			tbl += "<tr>";
+			for (let i = 0; i < siz; i++) {
+				tbl += "<td>" + itm[cols[i]] + "</td>";
+			}
+			tbl += "</tr>";
+		});
+	}
+	tbl += "</tbody></table>";
+	return tbl;
+}
+
+function getRecord(data,) {
+	var frm = "<form>";
+	if (Array.isArray(data)) {
+		let r = data[0];
+		for (let col in r) {
+			frm += "<label for='" + col + "'>" + col + "</label><input name='" + col + "/><br/>";
+		}
+	}
+	frm += "</form>";
+	return frm;
+}
